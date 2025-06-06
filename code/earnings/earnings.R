ae324d546cfa62f0f19b00e0b6482cdcd1c7c10b
gbekes <bekesg@gmail.com>
2025-06-06 18:04:59 +0200

    week00

---
+++ code/earnings/earnings.R
@@ -0,0 +1,34 @@
+# open data morg in not shared folder
+
+library(tidyverse)
+
+path="not-shared/"
+morg <- read_csv(paste0(path, "morg-2014-emp.csv"))
+
+# ordered freq table of state
+morg %>%
+  group_by(state) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  mutate(state = fct_reorder(state, n)) %>%
+  ggplot(aes(x = state, y = n)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "Number of Employees by State in Morg",
+       x = "State",
+       y = "Number of Employees") +
+  theme_minimal()
+
+# find the largest state in terms of number of obs
+
+morg2=morg %>%
+  group_by(state) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  slice(1) %>%
+  pull(state)
+
+
+# filter morg for the largest state
+morg_largest_state <- morg %>%
+  filter(state == morg2)
